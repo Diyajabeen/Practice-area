3e5b3f9d2cc2adedab970f25161dc8a8118961b6
diya jabeen <diyajabeen375@gmail.com>
2025-07-19 20:07:15 +0530

    day 3 tasks: input check + CSS demo

---
+++ prog3.py
@@ -0,0 +1,9 @@
+'''input writing and print pos or neg or zero'''
+num=int(input("enter any number"))
+if num>0:
+    print("Number is positive")
+elif num<0:
+    print("Number is negative")
+else:
+    print("Number is zero")
+    
